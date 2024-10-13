7faed2902d69059ec209ffa99954b6bf0d5cc47e
DylanQL <angelo.quispe.l@tecsup.edu.pe>
2024-10-12 20:31:06 -0500

    commit

--- EasyTrack/settings.py
+++ EasyTrack/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'sistema.apps.SistemaConfig',
+    'Sistema.apps.SistemaConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
